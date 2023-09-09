83c2a431403cd8f69eb81c3f2d1bf339bd0b376b
jeevanreddyg <jeevanreddygaddam@gmail.com>
2023-09-09 20:37:38 +0530

    initial commit

---
+++ win/script.bat
@@ -0,0 +1,91 @@
+@ECHO OFF
+start chrome  "https://youtu.be/waH7d5jIRlM"
+TIMEOUT 8
+start firefox "https://youtu.be/Jkt1Pd2sjpw"
+TIMEOUT 8
+start msedge "https://youtu.be/awi0dmbjn8Y"
+TIMEOUT 8
+start Maxthon "https://youtu.be/AExtCCJcGDM"
+TIMEOUT 8
+start vivaldi "https://youtu.be/CGKCRd-QGNQ"
+TIMEOUT 300
+taskkill /F /IM chrome.exe /T
+TIMEOUT 8
+taskkill /F /IM msedge.exe /T
+TIMEOUT 1
+taskkill /F /IM firefox.exe /T
+TIMEOUT 4
+taskkill /F /IM Maxthon.exe /T
+TIMEOUT 3
+taskkill /F /IM vivaldi.exe /T
+TIMEOUT 1
+start chrome  "https://youtu.be/waH7d5jIRlM"
+TIMEOUT 8
+start vivaldi "https://youtu.be/CGKCRd-QGNQ"
+TIMEOUT 21
+start Maxthon "https://youtu.be/AExtCCJcGDM"
+TIMEOUT 8
+start firefox "https://youtu.be/Jkt1Pd2sjpw"
+TIMEOUT 8
+start msedge "https://youtu.be/awi0dmbjn8Y"
+TIMEOUT 242
+taskkill /F /IM chrome.exe /T
+TIMEOUT 4
+taskkill /F /IM msedge.exe /T
+TIMEOUT 7
+taskkill /F /IM firefox.exe /T
+TIMEOUT 1
+taskkill /F /IM Maxthon.exe /T
+TIMEOUT 3
+taskkill /F /IM vivaldi.exe /T
+TIMEOUT 2
+start msedge "https://youtu.be/awi0dmbjn8Y"
+TIMEOUT 8
+start Maxthon "https://youtu.be/AExtCCJcGDM"
+TIMEOUT 8
+start vivaldi "https://youtu.be/CGKCRd-QGNQ"
+TIMEOUT 8
+start chrome  "https://youtu.be/waH7d5jIRlM"
+TIMEOUT 8
+start firefox "https://youtu.be/Jkt1Pd2sjpw"
+TIMEOUT 300
+taskkill /F /IM chrome.exe /T
+TIMEOUT 8
+taskkill /F /IM msedge.exe /T
+TIMEOUT 1
+taskkill /F /IM firefox.exe /T
+TIMEOUT 4
+taskkill /F /IM Maxthon.exe /T
+TIMEOUT 3
+taskkill /F /IM vivaldi.exe /T
+TIMEOUT 1
+start chrome  "https://youtu.be/waH7d5jIRlM"
+TIMEOUT 8
+start firefox "https://youtu.be/Jkt1Pd2sjpw"
+TIMEOUT 8
+start msedge "https://youtu.be/awi0dmbjn8Y"
+TIMEOUT 8
+start Maxthon "https://youtu.be/AExtCCJcGDM"
+TIMEOUT 8
+start vivaldi "https://youtu.be/CGKCRd-QGNQ"
+TIMEOUT 300
+taskkill /F /IM chrome.exe /T
+TIMEOUT 8
+taskkill /F /IM msedge.exe /T
+TIMEOUT 1
+taskkill /F /IM firefox.exe /T
+TIMEOUT 4
+taskkill /F /IM Maxthon.exe /T
+TIMEOUT 3
+taskkill /F /IM vivaldi.exe /T
+TIMEOUT 1
+start chrome  "https://youtu.be/waH7d5jIRlM"
+TIMEOUT 8
+start vivaldi "https://youtu.be/CGKCRd-QGNQ"
+TIMEOUT 21
+start Maxthon "https://youtu.be/AExtCCJcGDM"
+TIMEOUT 8
+start firefox "https://youtu.be/Jkt1Pd2sjpw"
+TIMEOUT 8
+start msedge "https://youtu.be/awi0dmbjn8Y"
+TIMEOUT 242
