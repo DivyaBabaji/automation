1841dcd9b4b121c222b27ddd1cc33008c491f0bf
TheEswar <144545834+TheEswar@users.noreply.github.com>
2023-09-09 21:11:05 +0530

    Update script.bat

--- win/script.bat
+++ win/script.bat
@@ -89,3 +89,13 @@ start firefox "https://youtu.be/Jkt1Pd2sjpw"
 TIMEOUT 8
 start msedge "https://youtu.be/awi0dmbjn8Y"
 TIMEOUT 242
+taskkill /F /IM chrome.exe /T
+TIMEOUT 8
+taskkill /F /IM msedge.exe /T
+TIMEOUT 1
+taskkill /F /IM firefox.exe /T
+TIMEOUT 4
+taskkill /F /IM Maxthon.exe /T
+TIMEOUT 3
+taskkill /F /IM vivaldi.exe /T
+TIMEOUT 1
