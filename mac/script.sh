243dc79051926e0e1ca7d649a24435919a0646b7
vinodkumarnv <nvvinodkumar123@gmail.com>
2023-09-11 20:38:46 +0530

    updated the script as Windows Batch script version

--- mac/script.sh
+++ mac/script.sh
@@ -1,21 +1,83 @@
 #! /bin/bash
-open -a opera  https://www.youtube.com/watch?v=TkReAF2d8n8
+open -a opera  https://youtu.be/awi0dmbjn8Y
 sleep 8
-open -a firefox https://www.youtube.com/watch?v=TkReAF2d8n8
+open -a firefox https://youtu.be/AExtCCJcGDM
 sleep 8
-open -a seamonkey https://www.youtube.com/watch?v=TkReAF2d8n8
+open -a safari https://youtu.be/CGKCRd-QGNQ
 sleep 8
-open -a vivaldi  https://www.youtube.com/watch?v=TkReAF2d8n8
+open -a Microsoft\ Edge.app  https://youtu.be/waH7d5jIRlM
 sleep 8
-open -a chromium https://www.youtube.com/watch?v=TkReAF2d8n8
+open -a chrome https://youtu.be/Jkt1Pd2sjpw
 sleep 300
 pkill -x opera
 sleep 8
 pkill -x firefox
+sleep 1
+pkill -x safari
+sleep 4
+pkill -x Microsoft\ Edge
+sleep 3
+pkill -x chrome
+sleep 1
+#iteration 2
+open -a opera  https://youtu.be/awi0dmbjn8Y
 sleep 8
-pkill -x seamonkey
+open -a chrome https://youtu.be/AExtCCJcGDM
+sleep 21
+open -a Microsoft\ Edge.app https://youtu.be/CGKCRd-QGNQ
 sleep 8
-pkill -x vivaldi
+open -a firefox  https://youtu.be/waH7d5jIRlM
 sleep 8
-pkill -x chromium
+open -a safari https://youtu.be/Jkt1Pd2sjpw
+sleep 242
+pkill -x opera
+sleep 4
+pkill -x chrome
+sleep 1
+pkill -x Microsoft\ Edge
+sleep 7
+pkill -x firefox
+sleep 3
+pkill -x safari
+sleep 2
+#iteration 3
+open -a firefox  https://youtu.be/awi0dmbjn8Y
 sleep 8
+open -a safari https://youtu.be/AExtCCJcGDM
+sleep 8
+open -a opera https://youtu.be/CGKCRd-QGNQ
+sleep 8
+open -a chrome  https://youtu.be/waH7d5jIRlM
+sleep 8
+open -a Microsoft\ Edge.app https://youtu.be/Jkt1Pd2sjpw
+sleep 300
+pkill -x firefox
+sleep 8
+pkill -x safari
+sleep 1
+pkill -x opera
+sleep 4
+pkill -x chrome
+sleep 3
+pkill -x Microsoft\ Edge
+sleep 1
+#iteration 4
+open -a firefox  https://youtu.be/awi0dmbjn8Y
+sleep 8
+open -a Microsoft\ Edge.app https://youtu.be/AExtCCJcGDM
+sleep 8
+open -a chrome https://youtu.be/CGKCRd-QGNQ
+sleep 8
+open -a safari  https://youtu.be/waH7d5jIRlM
+sleep 8
+open -a opera https://youtu.be/Jkt1Pd2sjpw
+sleep 300
+pkill -x firefox
+sleep 8
+pkill -x Microsoft\ Edge
+sleep 1
+pkill -x chrome
+sleep 4
+pkill -x safari
+sleep 3
+pkill -x opera
